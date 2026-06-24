3fb15f2c10cbe2557eee93bc21ed1b651e5b11b0
Henrique Valgas <henryvalgas@gmail.com>
2026-06-23 22:06:46 -0300

    Inicio do projeto oficina-mecanica-api

---
+++ src/main/java/com/oficinamecanica/oficina_mecanica_api/OficinaMecanicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.oficinamecanica.oficina_mecanica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaMecanicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaMecanicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oficinamecanica/oficina_mecanica_api/OficinaMecanicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficinamecanica.oficina_mecanica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaMecanicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
