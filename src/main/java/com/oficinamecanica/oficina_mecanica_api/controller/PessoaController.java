0fbf133c09ed2b3e19397370443b0f78f6b1b232
HENRIQUE VALGAS <henryvalgas@gmail.com>
2026-09-05 22:57:10 -0300

    remove: remocao classe builder pessoa e  pacote builder

--- src/main/java/com/oficinamecanica/oficina_mecanica_api/controller/PessoaController.java
+++ src/main/java/com/oficinamecanica/oficina_mecanica_api/controller/PessoaController.java
@@ -1,6 +1,5 @@
 package com.oficinamecanica.oficina_mecanica_api.controller;
 
-import com.oficinamecanica.oficina_mecanica_api.builder.PessoaBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
